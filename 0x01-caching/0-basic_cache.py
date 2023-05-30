8efd59bdbd834ec28f7fc5990da30e5d4f5d7af3
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-30 20:40:05 +0100

    Added files for task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Module for task 1
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Simple caching system
+    """
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Store data in cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get data from cache
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        else:
+            return None
